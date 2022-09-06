5e504601a3a6ca2b98f9e180e9ffffa199ddb114
Dongting Cai <cai00151@morris.umn.edu>
2022-09-06 16:00:30 -0500

    Finish big_clean.sh, with test (should) pass
    < Issue #2 >
    Finish Extract and Compile.sh, with test should pass - Mac do not understand the `grep -P`, we will let Github Action take care of that.
    
    Co-authored-by: Yubo Mao <mao00071@morris.umn.edu>

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -4,14 +4,14 @@
 tempDir=$(mktemp -d)
 
 # Extracts file to temp directory
-tar zxvf "$num" --directory "$tempDir"
+tar zxf "$1" --directory "$tempDir"
 
 # Move to extracted directory
-argument=$(basename "$num" .tgz)
-cd "$tempDir/$argument" || exit
+arguNum=$(basename "$1" .tgz)
+cd "$tempDir/$arguNum" || exit
 
 # Delete all files with "DELETE ME!"
-grep -l 'DELETE ME!' ./* | xrags rm -f
+grep -l 'DELETE ME!' ./* | xargs rm -f
 
 # Create new clean archive
-tar czf "$oldArch"/cleaned_"$num" -C ./.. "$argument"
+tar czf "$OLDPWD"/cleaned_"$1" -C ./.. "$arguNum"
